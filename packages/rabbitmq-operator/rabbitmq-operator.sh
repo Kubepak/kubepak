42ca728194da6a6a676d0a3adf8f417b3f27ba53
Florent CARRÉ <fcarre@generixgroup.com>
2024-05-03 11:17:23 -0500

    feat: add rabbitmq-operator

---
+++ packages/rabbitmq-operator/rabbitmq-operator.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+#
+#  This file is part of Kubepak.
+#
+#  Kubepak is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Kubepak is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Kubepak.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+set -eo pipefail
+
+source "support/scripts/package.sh"
+
+#-----------------------------------------------------------------------------
+# Package Options
+
+# @package-option attributes="final"
+# @package-option attributes="shared"
+
+# @package-option weak-dependencies="emissary-ingress"
+
+#-----------------------------------------------------------------------------
+# Private Constants
+
+readonly __RABBITMQ_OPERATOR_CHART_VERSION="4.3.1"
+
+#-----------------------------------------------------------------------------
+# Public Hooks
+
+hook_initialize() {
+    package_cache_values_file_write ".packages.${PACKAGE_IPATH}.chartVersion" "${__RABBITMQ_OPERATOR_CHART_VERSION}"
+
+    k8s_namespace_create "${K8S_PACKAGE_NAMESPACE}"
+
+    registry_credentials_add_namespace "${K8S_PACKAGE_NAMESPACE}"
+}
+
+hook_install() {
+    package_helm_install "${K8S_PACKAGE_NAME}" "${K8S_PACKAGE_NAMESPACE}" "${PACKAGE_DIR}/files/helm-chart"
+
+    argo_cd_application_wait "${K8S_PACKAGE_NAME}"
+}
+
+hook_upgrade() {
+    hook_install
+}
+
+package_hook_execute "${@}"
